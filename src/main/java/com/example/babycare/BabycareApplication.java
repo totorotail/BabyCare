33ca391d84c357088c842f967f075bd168d023a8
totorotail <jminkyun@gmail.com>
2024-06-23 14:53:14 +0900

    initial commit

---
+++ src/main/java/com/example/babycare/BabycareApplication.java
@@ -0,0 +1,13 @@
+package com.example.babycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabycareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabycareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/babycare/BabycareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.babycare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabycareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
